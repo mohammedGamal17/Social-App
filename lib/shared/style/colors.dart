b7112eca395dac4fcc8eebb5db902c97c4cfda9e
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-30 01:07:27 +0200

    Update colors.dart

---
+++ lib/shared/style/colors.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final textColor = HexColor('007FFF');
+final iconColor = HexColor('007FFF');
+final buttonColor = HexColor('007FFF');
+final separatorColor = HexColor('007FFF');
+
+final appBarLightColor = HexColor('CCE5FF');
+final bottomNavigationBarLightColor = HexColor('CCE5FF');
+final selectedIconLightColor = HexColor('CCE5FF');
+final unselectedIconLightColor = HexColor('99CCFF');
+
+final appBarDarkColor = HexColor('001933');
+final bottomNavigationBarDarkColor = HexColor('001933');
+final selectedIconDarkColor = HexColor('001933');
+final unselectedIconDarkColor = HexColor('003366');
+
+final lightBackground = HexColor('CCE5FF');
+final darkBackground = HexColor('001933');
+
+final gradientColor1 = HexColor('66B2FF');
+final gradientColor2 = HexColor('004C99');

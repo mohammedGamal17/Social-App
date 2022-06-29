a67899bd9e4c28abdda2a1a175547e7884e863cc
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-29 19:10:43 +0200

    Update assets_res.dart

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -3,6 +3,5 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: lines_longer_than_80_chars
 class AssetsRes {
-  static const String LOGO_SOCIAL_NETWORK = 'assets/Images/logo-social-network.webp';
   static const String LOGO = 'assets/Images/logo.png';
 }

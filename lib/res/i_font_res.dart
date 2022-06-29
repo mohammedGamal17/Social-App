5b1d41a339cc20acba5729055ae3cc4b1e2d5fda
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-29 19:10:41 +0200

    Update i_font_res.dart

--- lib/res/i_font_res.dart
+++ lib/res/i_font_res.dart
@@ -3,10 +3,5 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: lines_longer_than_80_chars
 class FontRes {
-  static const String ROBOTOCONDENSED_BOLD = 'RobotoCondensed-Bold';
-  static const String ROBOTOCONDENSED_BOLDITALIC = 'RobotoCondensed-BoldItalic';
-  static const String ROBOTOCONDENSED_ITALIC = 'RobotoCondensed-Italic';
-  static const String ROBOTOCONDENSED_LIGHT = 'RobotoCondensed-Light';
-  static const String ROBOTOCONDENSED_LIGHTITALIC = 'RobotoCondensed-LightItalic';
   static const String ROBOTOCONDENSED = 'RobotoCondensed';
 }

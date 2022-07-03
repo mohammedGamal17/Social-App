4766c0596b42669883eaca662574d47cd9784a1e
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-03 16:08:34 +0200

    Update home.dart

--- lib/layout/home.dart
+++ lib/layout/home.dart
@@ -1,30 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social/shared/cubit/app_cubit/app_cubit.dart';
+import 'package:social/shared/style/theme_service.dart';
 
-import '../shared/style/theme_service.dart';
+import '../shared/cubit/app_cubit/app_states.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-            onPressed: () {
-              ThemeService().changeTheme();
-              print('dark mode');
-            },
-            icon: const Icon(Icons.dark_mode_outlined),
-          ),
-        ],
-      ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {
-          },
-          icon: const Icon(Icons.account_circle_outlined),
-        ),
+    return BlocProvider(
+      create: (context) => AppCubit(),
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    ThemeService().changeTheme();
+                  },
+                  icon: const Icon(Icons.dark_mode_outlined),
+                ),
+              ],
+            ),
+            body: Center(
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.account_circle_outlined),
+              ),
+            ),
+          );
+        },
       ),
     );
   }

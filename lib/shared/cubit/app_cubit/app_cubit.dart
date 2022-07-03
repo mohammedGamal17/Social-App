1005a2a7ad29e5d551e3058f497fa0166dd56d08
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-03 16:08:36 +0200

    Update app_cubit.dart

--- lib/shared/cubit/app_cubit/app_cubit.dart
+++ lib/shared/cubit/app_cubit/app_cubit.dart
@@ -2,9 +2,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social/shared/cubit/app_cubit/app_states.dart';
 
 import '../../networks/remote/dio_helper.dart';
+import '../../style/theme_service.dart';
 
-class AppCubit extends Cubit<AppStates>{
-  AppCubit():super(AppInit());
+class AppCubit extends Cubit<AppStates> {
+  AppCubit() : super(AppInit());
 
   static AppCubit get(context) => BlocProvider.of(context);
 

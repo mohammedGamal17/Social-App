148142832af69bf0d1f75a5199674585543d47f1
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-07 17:19:46 +0200

    Update assets.dart

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -4,5 +4,9 @@ class Assets {
 
   static const String FontsRobotoCondensed = 'assets/Fonts/RobotoCondensed.ttf';
   static const String ImagesLogo = 'assets/Images/logo.png';
+  static const String ImagesOnboarding1 = 'assets/Images/onboarding1.gif';
+  static const String ImagesOnboarding2 = 'assets/Images/onboarding2.gif';
+  static const String ImagesOnboarding3 = 'assets/Images/onboarding3.gif';
+  static const String ImagesOnboarding4 = 'assets/Images/onboarding4.gif';
 
 }

a6646eb25126712a3c6969eb1d019083af836963
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-07 16:53:52 +0200

    Create onboard_model.dart

---
+++ lib/models/onboard_model.dart
@@ -0,0 +1,8 @@
+class BoardingModel {
+  final String imgUrl;
+  final String title;
+  final String body;
+
+  BoardingModel(
+      {required this.imgUrl, required this.title, required this.body});
+}

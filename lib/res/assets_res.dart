618a3bf353f398479f43118dc8a2c2f300a39129
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-08 00:17:17 +0200

    Update assets_res.dart

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -3,10 +3,11 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: lines_longer_than_80_chars
 class AssetsRes {
-  static const String 1 = 'assets/Images/1.svg';
   static const String LOGO = 'assets/Images/logo.png';
+  static const String LOGOPAGE = 'assets/Images/logopage.png';
   static const String ONBOARDING1 = 'assets/Images/onboarding1.gif';
   static const String ONBOARDING2 = 'assets/Images/onboarding2.gif';
   static const String ONBOARDING3 = 'assets/Images/onboarding3.gif';
   static const String ONBOARDING4 = 'assets/Images/onboarding4.gif';
+  static const String SAVAG = 'assets/Images/savag.svg';
 }

dad151ae628511b839ee66b2ecf011024b961a39
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-08 01:37:41 +0200

    Update register_screen.dart

--- lib/modules/auth_screens/register_screen.dart
+++ lib/modules/auth_screens/register_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:social/modules/auth_screens/login_screen.dart';
 import 'package:social/shared/cubit/auth_cubit/auth_cubit.dart';
 
+import '../../shared/components/components.dart';
 import '../../shared/cubit/auth_cubit/auth_states.dart';
+import '../../shared/style/colors.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+  RegisterScreen({super.key});
+
+  final formKey = GlobalKey<FormState>();
+
+  final name = TextEditingController();
+  final email = TextEditingController();
+  final password = TextEditingController();
+  final phone = TextEditingController();
+
+  final isPassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -14,11 +28,179 @@ class RegisterScreen extends StatelessWidget {
       child: BlocConsumer<AuthCubit, AuthStates>(
         listener: (context, state) {},
         builder: (context, state) {
+          AuthCubit cubit = AuthCubit.get(context);
           return Scaffold(
-            appBar: AppBar(),
-            body: const SingleChildScrollView(
+            body: Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/Images/regpage.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
               child: Center(
-                child: Text('RegisterScreen'),
+                child: SingleChildScrollView(
+                  child: Stack(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Form(
+                          key: formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'SIGN UP',
+                                style: Theme.of(context).textTheme.headline5,
+                              ),
+                              const SizedBox(height: 15.0),
+                              textFormField(
+                                controller: name,
+                                validate: (value) {
+                                  if (value.isEmpty ||
+                                      value.length < 4 ||
+                                      value.length > 50) {
+                                    return 'Please Enter Yor Name';
+                                  }
+                                  return null;
+                                },
+                                labelText: 'Name',
+                                prefix: Icons.account_circle_outlined,
+                                borderRadius: 15.0,
+                                autoFocus: true,
+                                textInputType: TextInputType.name,
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              textFormField(
+                                controller: email,
+                                validate: (value) {
+                                  if (value.isEmpty ||
+                                      value.length < 14 ||
+                                      value.length > 50) {
+                                    return 'Please Enter Valid Email';
+                                  }
+                                  return null;
+                                },
+                                labelText: 'Email',
+                                prefix: Icons.email_outlined,
+                                borderRadius: 20.0,
+                                autoFocus: true,
+                                textInputType: TextInputType.emailAddress,
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              textFormField(
+                                controller: phone,
+                                validate: (value) {
+                                  if (value.isEmpty ||
+                                      value.length < 11 ||
+                                      value.length > 50) {
+                                    return 'Please Enter Valid Phone';
+                                  }
+                                  return null;
+                                },
+                                labelText: 'Phone',
+                                prefix: Icons.phone,
+                                borderRadius: 15.0,
+                                autoFocus: true,
+                                textInputType: TextInputType.phone,
+                              ),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              textFormField(
+                                  controller: password,
+                                  validate: (value) {
+                                    if (value.isEmpty ||
+                                        value.length < 8 ||
+                                        value.length > 50) {
+                                      return 'Please Enter Valid Password';
+                                    }
+                                    return null;
+                                  },
+                                  labelText: 'Password',
+                                  prefix: Icons.lock_outline,
+                                  borderRadius: 20.0,
+                                  textInputType: TextInputType.visiblePassword,
+                                  isPassword: cubit.isPassword,
+                                  suffix: cubit.suffix,
+                                  suffixOnTap: () {
+                                    cubit.changePasswordVisibility();
+                                  },
+                                  onSubmit: (value) {
+                                    if (formKey.currentState!.validate()) {
+                                      /*cubit.userLogin(
+                                      email: emailController.text,
+                                      password: passwordController.text,
+                                    );*/
+                                      return navigateToAndReplace(
+                                        context,
+                                        LoginScreen(),
+                                      );
+                                    }
+                                  }),
+                              const SizedBox(
+                                height: 10.0,
+                              ),
+                              state is! RegisterLoadingState
+                                  ? InkWell(
+                                      onTap: () {
+                                        if (formKey.currentState!.validate()) {
+                                          /*cubit.userLogin(
+                                    email: emailController.text,
+                                    password: passwordController.text,
+                                  );*/
+                                          return navigateToAndReplace(
+                                            context,
+                                            LoginScreen(),
+                                          );
+                                        }
+                                      },
+                                      child: Center(
+                                        child: Container(
+                                          height: 40.0,
+                                          width: double.infinity,
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                            color: buttonColor,
+                                            gradient: LinearGradient(
+                                              begin:
+                                                  AlignmentDirectional.topStart,
+                                              end: AlignmentDirectional
+                                                  .bottomEnd,
+                                              colors: [
+                                                HexColor('CCE5FF'),
+                                                HexColor('007FFF'),
+                                              ],
+                                            ),
+                                          ),
+                                          child: Text(
+                                            'SIGN UP'.capitalize!,
+                                            style: TextStyle(
+                                              fontSize: 16.0,
+                                              color: HexColor('004C99'),
+                                              fontWeight: FontWeight.bold,
+                                              fontFamily: 'RobotoCondensed',
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  : Center(child: circularProgressIndicator()),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           );

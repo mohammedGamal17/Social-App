7dce29d13cc86396d83d81c864c095a8aa157a72
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-07 16:49:47 +0200

    Create auth_cubit.dart

---
+++ lib/shared/cubit/auth_cubit/auth_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'auth_states.dart';
+
+class AuthCubit extends Cubit<AuthenticationState> {
+  AuthCubit() : super(AuthInit());
+
+  static AuthCubit get(context) => BlocProvider.of(context);
+  bool isPassword = true;
+  IconData suffix = Icons.visibility_outlined;
+
+  void changePasswordVisibility() {
+    isPassword = !isPassword;
+    suffix =
+        isPassword ? Icons.visibility_off_outlined : Icons.visibility_outlined;
+    emit(PasswordVisibilityState());
+  }
+}

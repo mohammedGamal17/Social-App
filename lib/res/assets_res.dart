baae55d646b0328242bebc0525ac0fa1a9d52d92
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-07 17:19:32 +0200

    Update assets_res.dart

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -4,4 +4,8 @@
 // ignore_for_file: lines_longer_than_80_chars
 class AssetsRes {
   static const String LOGO = 'assets/Images/logo.png';
+  static const String ONBOARDING1 = 'assets/Images/onboarding1.gif';
+  static const String ONBOARDING2 = 'assets/Images/onboarding2.gif';
+  static const String ONBOARDING3 = 'assets/Images/onboarding3.gif';
+  static const String ONBOARDING4 = 'assets/Images/onboarding4.gif';
 }

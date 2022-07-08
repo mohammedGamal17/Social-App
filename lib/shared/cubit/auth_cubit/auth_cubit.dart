06250c123ba424e5ad5465407e9598ef11d7c3b1
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-08 19:14:05 +0200

    Update auth_cubit.dart

--- lib/shared/cubit/auth_cubit/auth_cubit.dart
+++ lib/shared/cubit/auth_cubit/auth_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:social/shared/components/components.dart';
 
 import 'auth_states.dart';
 
@@ -9,6 +11,86 @@ class AuthCubit extends Cubit<AuthStates> {
   static AuthCubit get(context) => BlocProvider.of(context);
   bool isPassword = true;
   IconData suffix = Icons.visibility_outlined;
+
+  void userRegister(
+    context, {
+    required String name,
+    required String email,
+    required String password,
+    required String phone,
+  }) async {
+    emit(RegisterLoadingState());
+    /*try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(RegisterSuccessState());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        snack(context, content: 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        snack(context, content: 'The account already exists for that email.');
+      }
+    } catch (e) {
+      emit(RegisterFailState());
+      snack(context, content: e.toString());
+    }*/
+    final credential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    )
+        .then(
+      (value) {
+        snack(context, content: '${value.credential}');
+        emit(RegisterSuccessState());
+      },
+    ).catchError(
+      (onError) {
+        emit(RegisterFailState());
+        snack(context, content: onError.toString(), bgColor: Colors.red);
+      },
+    );
+  }
+
+  void userLogin(
+    context, {
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoadingState());
+    /*try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      emit(LoginSuccessState());
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        snack(context, content: 'No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        snack(context, content: 'Wrong password provided for that user.');
+      }
+    } catch (e) {
+      emit(LoginFailState());
+      snack(context, content: e.toString());
+    }*/
+
+    final credential = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    )
+        .then((value) {
+      snack(context, content: '${value.user}');
+      emit(LoginSuccessState());
+    }).catchError((onError) {
+      emit(LoginFailState());
+      snack(context, content: onError.toString(), bgColor: Colors.red);
+    });
+  }
 
   void changePasswordVisibility() {
     isPassword = !isPassword;

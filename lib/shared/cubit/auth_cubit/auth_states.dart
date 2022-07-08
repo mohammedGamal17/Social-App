67c0e0427518d99c70c0a24c7e710ba1760a0b59
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-08 23:39:54 +0200

    Update auth_states.dart

--- lib/shared/cubit/auth_cubit/auth_states.dart
+++ lib/shared/cubit/auth_cubit/auth_states.dart
@@ -6,12 +6,30 @@ class LoginLoadingState extends AuthStates {}
 
 class LoginSuccessState extends AuthStates {}
 
-class LoginFailState extends AuthStates {}
+class LoginFailState extends AuthStates {
+  final String error;
+
+  LoginFailState(this.error);
+}
 
 class RegisterLoadingState extends AuthStates {}
 
 class RegisterSuccessState extends AuthStates {}
 
-class RegisterFailState extends AuthStates {}
+class RegisterFailState extends AuthStates {
+  final String error;
+
+  RegisterFailState(this.error);
+}
+
+class CreateUserLoadingState extends AuthStates {}
+
+class CreateUserSuccessState extends AuthStates {}
+
+class CreateUserFailState extends AuthStates {
+  final String error;
+
+  CreateUserFailState(this.error);
+}
 
 class PasswordVisibilityState extends AuthStates {}

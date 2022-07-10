2cde63257cdac112f9a809607c6eed3f6334e471
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-10 16:34:14 +0200

    Update constants.dart

--- lib/shared/components/constants.dart
+++ lib/shared/components/constants.dart
@@ -1,3 +1,10 @@
 // SHA-1 = 1:330037147852:android:54182adb6c761890ab6a69
 
+import '../../main.dart';
+
 const baseUrl = '';
+//token is in login.dart
+String? uId = sharedPreferences.getString('isLogin');
+
+//onBoarding is in PageViewScreen.dart
+bool? onBoarding = sharedPreferences.getBool('skip');

8c9ce35fbdbbbadaf939ad1a18c9d284e03966f0
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-10 19:28:28 +0200

    Update home_screen.dart

--- lib/modules/home_screen.dart
+++ lib/modules/home_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social/models/user_model/user_model.dart';
 import 'package:social/shared/components/components.dart';
-import 'package:social/shared/components/constants.dart';
 import 'package:social/shared/cubit/app_cubit/app_cubit.dart';
 
 import '../shared/cubit/app_cubit/app_states.dart';
+import '../shared/style/theme_service.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,14 +19,39 @@ class HomeScreen extends StatelessWidget {
         builder: (context, state) {
           AppCubit cubit = AppCubit.get(context);
           return Scaffold(
+            appBar: AppBar(
+              title: const Text('Home page'),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    ThemeService().changeTheme();
+                    cubit.changeThemeMode();
+                  },
+                  icon: const Icon(Icons.dark_mode_outlined),
+                )
+              ],
+            ),
             body: cubit.userModel != null
-                ? Center(
-                    child: Text(uId!),
-                  )
+                ? pageBuilder(context, cubit.userModel!)
                 : circularProgressIndicator(),
           );
         },
       ),
+    );
+  }
+
+  Widget pageBuilder(context, UserModel model) {
+    return Column(
+      children: [
+        if (model.isEmailVerified == false)
+          alertMessage(
+            context,
+            data: 'You Need To Verification Your Account',
+            buttonText: 'Send ',
+            onPressed: () {},
+          ),
+        Text(model.uId!),
+      ],
     );
   }
 }

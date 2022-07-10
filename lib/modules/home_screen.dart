c2b84a677fba706ce244fb31c1194767dedf4f8f
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-10 16:34:12 +0200

    Update home_screen.dart

--- lib/modules/home_screen.dart
+++ lib/modules/home_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:social/shared/components/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return  Scaffold(
       body: Center(
-        child: Text('data'),
+        child: Text(uId!),
       ),
     );
   }

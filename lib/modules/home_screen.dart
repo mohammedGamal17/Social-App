cc605438bd0e4af6091a287961af8ed82e3306d2
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-10 18:28:11 +0200

    Update home_screen.dart

--- lib/modules/home_screen.dart
+++ lib/modules/home_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social/shared/components/components.dart';
 import 'package:social/shared/components/constants.dart';
+import 'package:social/shared/cubit/app_cubit/app_cubit.dart';
+
+import '../shared/cubit/app_cubit/app_states.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: Text(uId!),
+    return BlocProvider(
+      create: (context) => AppCubit()..getUserData(context),
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          AppCubit cubit = AppCubit.get(context);
+          return Scaffold(
+            body: cubit.userModel != null
+                ? Center(
+                    child: Text(uId!),
+                  )
+                : circularProgressIndicator(),
+          );
+        },
       ),
     );
   }

2521675723c1e2da9d41f158d1b79eb2c03a2b06
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-10 18:28:03 +0200

    Update app_cubit.dart

--- lib/shared/cubit/app_cubit/app_cubit.dart
+++ lib/shared/cubit/app_cubit/app_cubit.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:social/shared/components/components.dart';
+import 'package:social/shared/components/constants.dart';
 import 'package:social/shared/cubit/app_cubit/app_states.dart';
 
+import '../../../models/user_model/user_model.dart';
 import '../../networks/remote/dio_helper.dart';
 import '../../style/theme_service.dart';
 
@@ -10,4 +16,18 @@ class AppCubit extends Cubit<AppStates> {
   static AppCubit get(context) => BlocProvider.of(context);
 
   DioHelper dio = DioHelper();
+
+  UserModel? userModel;
+
+  void getUserData(context) {
+    emit(GetHomeDataLoading());
+    FirebaseFirestore.instance.collection('users').doc(uId).get().then((value) {
+      userModel = UserModel.fromJson(value.data()!);
+      snack(context, content: 'Welcome ${userModel?.name}'.capitalize!);
+      emit(GetHomeDataSuccess());
+    }).catchError((onError) {
+      snack(context, content: onError.toString(), bgColor: Colors.red);
+      emit(GetHomeDataFail(onError.toString()));
+    });
+  }
 }

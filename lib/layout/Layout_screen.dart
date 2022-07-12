c1b7df81232bbf5accb8156fc575c07a62d05822
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-12 03:33:12 +0200

    Create Layout_screen.dart

---
+++ lib/layout/Layout_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social/shared/cubit/app_cubit/app_cubit.dart';
+import '../shared/cubit/app_cubit/app_states.dart';
+import '../shared/style/theme_service.dart';
+
+class LayOutScreen extends StatelessWidget {
+  const LayOutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AppCubit(),
+      child: BlocConsumer<AppCubit, AppStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          AppCubit cubit = AppCubit.get(context);
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(cubit.title[cubit.currentIndex]),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    ThemeService().changeTheme();
+                    cubit.changeThemeMode();
+                  },
+                  icon: const Icon(Icons.dark_mode_outlined),
+                )
+              ],
+            ),
+            body: cubit.screen[cubit.currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              items: cubit.navBarItem,
+              currentIndex: cubit.currentIndex,
+              type: BottomNavigationBarType.fixed,
+              onTap: (index) {
+                cubit.navBarChange(index);
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}

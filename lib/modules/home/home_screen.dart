342aa12d0fd7e4bebabd3dbc37cb4cd501ee7674
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-12 03:33:29 +0200

    Update home_screen.dart

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,12 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:social/models/user_model/user_model.dart';
-import 'package:social/shared/components/components.dart';
 import 'package:social/shared/cubit/app_cubit/app_cubit.dart';
+import 'package:social/shared/cubit/app_cubit/app_states.dart';
 
-import '../../shared/cubit/app_cubit/app_states.dart';
-import '../../shared/style/theme_service.dart';
+import '../../models/user_model/user_model.dart';
+import '../../shared/components/components.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,36 +13,49 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AppCubit(),
+      create: (context) => AppCubit()..getUserData(context),
       child: BlocConsumer<AppCubit, AppStates>(
         listener: (context, state) {},
         builder: (context, state) {
           AppCubit cubit = AppCubit.get(context);
           return Scaffold(
-            appBar: AppBar(
-              title: Text(cubit.title[cubit.currentIndex]),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    ThemeService().changeTheme();
-                    cubit.changeThemeMode();
-                  },
-                  icon: const Icon(Icons.dark_mode_outlined),
-                )
-              ],
-            ),
-            body: cubit.screen[cubit.currentIndex],
-            bottomNavigationBar: BottomNavigationBar(
-              items: cubit.navBarItem,
-              currentIndex: cubit.currentIndex,
-              type: BottomNavigationBarType.fixed,
-              onTap: (index) {
-                cubit.navBarChange(index);
-              },
-            ),
+            body: cubit.userModel != null
+                ? pageBuilder(context, cubit.userModel!)
+                : loadingAnimation(context),
           );
         },
       ),
     );
+  }
+
+  Widget pageBuilder(context, UserModel model) {
+    bool? isVerified = FirebaseAuth.instance.currentUser?.emailVerified;
+    return Column(
+      children: [
+        if (isVerified == false)
+          alertMessage(
+            context,
+            data: 'You Need To Verification Your Account',
+            buttonText: 'Send ',
+            onPressed: () {
+              firebaseSendNotification(context);
+            },
+          ),
+        Center(child: const Text('Home')),
+      ],
+    );
+  }
+
+  Future<void>? firebaseSendNotification(context){
+    return FirebaseAuth.instance.currentUser
+        ?.sendEmailVerification()
+        .then((value) {
+      snack(context,
+          content: 'Code Send Successfully Check Your Mail');
+    }).catchError((onError) {
+      snack(context,
+          content: 'Code Did Not Send !! ${onError.toString()}',
+          bgColor: Colors.red);
+    });
   }
 }

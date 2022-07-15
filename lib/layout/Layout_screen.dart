bd762aefa5e3336b6c49a0dd10e515639a723192
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-15 08:39:19 +0200

    Update Layout_screen.dart

--- lib/layout/Layout_screen.dart
+++ lib/layout/Layout_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:social/modules/search/search_screen.dart';
 import 'package:social/shared/components/components.dart';
 import 'package:social/shared/cubit/app_cubit/app_cubit.dart';

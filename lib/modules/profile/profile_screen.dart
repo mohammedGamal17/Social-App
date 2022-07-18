e2f048939f5c752921406f748e9d4aa828a9b67a
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-18 12:20:46 +0200

    Update profile_screen.dart

--- lib/modules/profile/profile_screen.dart
+++ lib/modules/profile/profile_screen.dart
@@ -16,150 +16,165 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AppCubit(),
+      create: (context) => AppCubit()..getUserData(context),
       child: BlocConsumer<AppCubit, AppStates>(
         listener: (context, state) {},
         builder: (context, state) {
           return Scaffold(
             appBar: AppBar(),
-            body: SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 220.0,
-                        width: double.infinity,
-                        child: Stack(
-                          alignment: AlignmentDirectional.bottomCenter,
-                          children: [
-                            Align(
-                              alignment: AlignmentDirectional.topCenter,
-                              child: SizedBox(
-                                height: 160.0,
-                                child: Image(
-                                  image: NetworkImage(
-                                    '${userModel.coverImage}',
+            body: AppCubit.get(context).userModel != null
+                ? SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 220.0,
+                            width: double.infinity,
+                            child: Stack(
+                              alignment: AlignmentDirectional.bottomCenter,
+                              children: [
+                                Align(
+                                  alignment: AlignmentDirectional.topCenter,
+                                  child: SizedBox(
+                                    height: 160.0,
+                                    child: Image(
+                                      image: NetworkImage(
+                                        '${userModel.coverImage}',
+                                      ),
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                  width: double.infinity,
-                                  fit: BoxFit.cover,
+                                ),
+                                CircleAvatar(
+                                  radius: 60.0,
+                                  backgroundColor:
+                                      Theme.of(context).scaffoldBackgroundColor,
+                                  child: CircleAvatar(
+                                    radius: 57.0,
+                                    backgroundImage:
+                                        NetworkImage('${userModel.image}'),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: double.infinity,
+                            alignment: AlignmentDirectional.center,
+                            child: Text(
+                                '${userModel.name} ${userModel.lastName}'
+                                    .capitalize!,
+                                style: Theme.of(context).textTheme.headline5),
+                          ),
+                          const SizedBox(height: 10.0),
+                          Container(
+                            width: double.infinity,
+                            alignment: AlignmentDirectional.center,
+                            child: Text('${userModel.bio}'.capitalize!,
+                                style: Theme.of(context).textTheme.caption),
+                          ),
+                          const SizedBox(height: 15.0),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Posts',
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                    ),
+                                    Text(
+                                      '10',
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    )
+                                  ],
                                 ),
                               ),
-                            ),
-                            CircleAvatar(
-                              radius: 60.0,
-                              backgroundColor:
-                                  Theme.of(context).scaffoldBackgroundColor,
-                              child: CircleAvatar(
-                                radius: 57.0,
-                                backgroundImage : NetworkImage('${userModel.image}'),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Photos',
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                    ),
+                                    Text(
+                                      '100',
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        alignment: AlignmentDirectional.center,
-                        child: Text(
-                            '${userModel.name} ${userModel.lastName}'
-                                .capitalize!,
-                            style: Theme.of(context).textTheme.headline5),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Container(
-                        width: double.infinity,
-                        alignment: AlignmentDirectional.center,
-                        child: Text('${userModel.bio}'.capitalize!,
-                            style: Theme.of(context).textTheme.caption),
-                      ),
-                      const SizedBox(height: 15.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Posts',
-                                  style: Theme.of(context).textTheme.bodyText2,
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Followers',
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                    ),
+                                    Text(
+                                      '10K',
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  '10',
-                                  style: Theme.of(context).textTheme.caption,
-                                )
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Photos',
-                                  style: Theme.of(context).textTheme.bodyText2,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      'Following',
+                                      style:
+                                          Theme.of(context).textTheme.bodyText2,
+                                    ),
+                                    Text(
+                                      '500',
+                                      style:
+                                          Theme.of(context).textTheme.caption,
+                                    )
+                                  ],
                                 ),
-                                Text(
-                                  '100',
-                                  style: Theme.of(context).textTheme.caption,
-                                )
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Followers',
-                                  style: Theme.of(context).textTheme.bodyText2,
+                          const SizedBox(height: 15.0),
+                          Row(
+                            children: [
+                              Expanded(
+                                child: OutlinedButton(
+                                  onPressed: () {},
+                                  child: Text(
+                                    'Add Photo',
+                                    style:
+                                        Theme.of(context).textTheme.bodyText2,
+                                  ),
                                 ),
-                                Text(
-                                  '10K',
-                                  style: Theme.of(context).textTheme.caption,
-                                )
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  'Following',
-                                  style: Theme.of(context).textTheme.bodyText2,
-                                ),
-                                Text(
-                                  '500',
-                                  style: Theme.of(context).textTheme.caption,
-                                )
-                              ],
-                            ),
-                          ),
+                              ),
+                              const SizedBox(width: 10.0),
+                              OutlinedButton(
+                                  onPressed: () {
+                                    navigateTo(
+                                      context,
+                                      EditProfileScreen(
+                                        userModel: userModel,
+                                      ),
+                                    );
+                                  },
+                                  child: const Icon(Icons.edit))
+                            ],
+                          )
                         ],
                       ),
-                      const SizedBox(height: 15.0),
-                      Row(
-                        children: [
-                          Expanded(
-                            child: OutlinedButton(
-                              onPressed: () {},
-                              child: Text('Add Photo',
-                                  style: Theme.of(context).textTheme.bodyText2),
-                            ),
-                          ),
-                          const SizedBox(width: 10.0),
-                          OutlinedButton(
-                              onPressed: () {
-                                navigateTo(
-                                    context,
-                                    EditProfileScreen(
-                                      userModel: userModel,
-                                    ));
-                              },
-                              child: const Icon(Icons.edit))
-                        ],
-                      )
-                    ],
-                  ),
-                )),
+                    ))
+                : loadingAnimation(context),
           );
         },
       ),

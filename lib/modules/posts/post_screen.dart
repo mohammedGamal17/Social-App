be33ce9164504361a50f796dc85be707fe965ba1
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-20 06:49:50 +0200

    Update post_screen.dart

--- lib/modules/posts/post_screen.dart
+++ lib/modules/posts/post_screen.dart
@@ -25,88 +25,81 @@ class PostScreen extends StatelessWidget {
         builder: (context, state) {
           return Scaffold(
             appBar: AppBar(),
-            body: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Card(
-                  shadowColor: const Color(0xFF0066CC),
-                  elevation: 3.0,
-                  color: const Color(0xFF66B2FF),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Row(
-                          children: [
-                            InkWell(
-                              child: CircleAvatar(
-                                radius: 25.0,
-                                backgroundImage:
-                                    NetworkImage('${userModel.image}'),
-                              ),
-                              onTap: () {
-                                navigateTo(
-                                  context,
-                                  ProfileScreen(
-                                    userModel: userModel,
-                                  ),
-                                );
-                              },
+            body: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Card(
+                shadowColor: const Color(0xFF0066CC),
+                elevation: 20.0,
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Row(
+                        children: [
+                          InkWell(
+                            child: CircleAvatar(
+                              radius: 25.0,
+                              backgroundImage:
+                                  NetworkImage('${userModel.image}'),
                             ),
-                            const SizedBox(width: 10.0),
-                            InkWell(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Text(
-                                      '${userModel.name} ${userModel.lastName}'
-                                          .capitalize!,
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .subtitle2),
-                                ],
-                              ),
-                              onTap: () {
-                                navigateTo(context,
-                                    ProfileScreen(userModel: userModel));
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                      separatorHorizontal(height: 0.5, opacity: 0.6),
-                      Card(
-                        shadowColor: const Color(0xFF0066CC),
-                        elevation: 3.0,
-                        color: const Color(0xFFCCE5FF),
-                        child: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Expanded(
-                            child: SizedBox(
-                              height: 350.0,
-                              child: TextField(
-                                decoration: const InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'What\'s on your mind?'),
-                                controller: post,
-                                keyboardType: TextInputType.text,
-                                maxLines: 16,
-                              ),
-                            ),
+                            onTap: () {
+                              navigateTo(
+                                context,
+                                ProfileScreen(
+                                  userModel: userModel,
+                                ),
+                              );
+                            },
                           ),
+                          const SizedBox(width: 10.0),
+                          InkWell(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${userModel.name} ${userModel.lastName}'
+                                      .capitalize!,
+                                  style: Theme.of(context).textTheme.bodyText1,
+                                ),
+                              ],
+                            ),
+                            onTap: () {
+                              navigateTo(
+                                  context, ProfileScreen(userModel: userModel));
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'What\'s on your mind ?',
+                            hintStyle: Theme.of(context).textTheme.bodyText2,
+                          ),
+                          style: Theme.of(context).textTheme.bodyText2,
+                          controller: post,
+                          keyboardType: TextInputType.text,
+                          maxLines: 1000,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: decorationButton(context,
-                            text: 'Post', borderRadius: 5.0, onTap: () {
-                          navigateTo(context, const LayOutScreen());
-                        }),
-                      )
-                    ],
-                  ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: decorationButton(context,
+                          text: 'Post', borderRadius: 5.0, onTap: () {
+                        navigateTo(context, const LayOutScreen());
+                      }),
+                    )
+                  ],
                 ),
               ),
             ),

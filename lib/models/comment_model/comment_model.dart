885beb74921accf03e6e2d6ccb0337946d806a8d
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-21 15:42:26 +0200

    Create comment_model.dart

---
+++ lib/models/comment_model/comment_model.dart
@@ -0,0 +1,55 @@
+class CommentModel {
+  late String? uId;
+  late String? name;
+  late String? lastName;
+  late String? email;
+  late String? image;
+  late String? dateTime;
+  late String? text;
+  late String? postImage;
+  late String? coverImage;
+  late String? bio;
+
+  CommentModel({
+    this.uId,
+    this.name,
+    this.lastName,
+    this.email,
+    this.dateTime,
+    this.text,
+    this.image,
+    this.postImage,
+    this.coverImage,
+    this.bio,
+  });
+
+  factory CommentModel.fromJson(Map<String, dynamic> json) {
+    return CommentModel(
+      uId: json['uId'],
+      name: json['name'],
+      lastName: json['lastName'],
+      email: json['email'],
+      image: json['image'],
+      dateTime: json['dateTime'],
+      postImage: json['postImage'],
+      text: json['text'],
+      coverImage: json['coverImage'],
+      bio: json['bio'],
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['uId'] = uId;
+    data['name'] = name;
+    data['lastName'] = lastName;
+    data['email'] = email;
+    data['image'] = image;
+    data['dateTime'] = dateTime;
+    data['postImage'] = postImage;
+    data['text'] = text;
+    data['coverImage'] = coverImage;
+    data['bio'] = bio;
+    return data;
+  }
+}

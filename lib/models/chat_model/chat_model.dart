a43f091f938d5111c617c37c271a24c019a5d273
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-07-25 16:51:01 +0200

    Create chat_model.dart

---
+++ lib/models/chat_model/chat_model.dart
@@ -0,0 +1,31 @@
+class ChatModel {
+  late String messageText;
+  late String senderId;
+  late String receiverId;
+  late String messageTime;
+
+  ChatModel({
+    required this.messageText,
+    required this.senderId,
+    required this.receiverId,
+    required this.messageTime,
+  });
+
+  factory ChatModel.fromJson(Map<String, dynamic> json) {
+    return ChatModel(
+      messageText: json['messageText'],
+      senderId: json['senderId'],
+      receiverId: json['receiverId'],
+      messageTime: json['messageTime'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['messageText'] = messageText;
+    data['senderId'] = senderId;
+    data['receiverId'] = receiverId;
+    data['messageTime'] = messageTime;  
+    return data;
+  }
+}

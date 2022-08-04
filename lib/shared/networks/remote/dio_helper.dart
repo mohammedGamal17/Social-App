4949dffa75859b9c95165da61be68c0fbd21c334
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-08-04 18:58:06 +0200

    Update dio_helper.dart

--- lib/shared/networks/remote/dio_helper.dart
+++ lib/shared/networks/remote/dio_helper.dart
@@ -9,7 +9,7 @@ class DioHelper {
   DioHelper() {
     dio = Dio(
       BaseOptions(
-        baseUrl: BASE_URL,
+        baseUrl: baseUrl,
         receiveDataWhenStatusError: true,
         connectTimeout: 20 * 1000,
         receiveTimeout: 20 * 1000,

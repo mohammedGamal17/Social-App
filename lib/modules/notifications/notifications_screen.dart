5e4356e83fec745aeb8fe0ea029245bba1a66999
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-08-18 15:53:36 +0200

    Update notifications_screen.dart

--- lib/modules/notifications/notifications_screen.dart
+++ lib/modules/notifications/notifications_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_sound/public/flutter_sound_recorder.dart';
 import 'package:social/shared/cubit/app_cubit/app_cubit.dart';
 import 'package:social/shared/cubit/app_cubit/app_states.dart';
 
@@ -9,14 +10,52 @@ class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => AppCubit(),
+      create: (context) => AppCubit()..initRecorder(),
       child: BlocConsumer<AppCubit, AppStates>(
         listener: (context, state) {},
         builder: (context, state) {
+          AppCubit cubit = AppCubit.get(context);
           return Scaffold(
-            body: Center(
-              child: Text('NotificationsScreen',
-                  style: Theme.of(context).textTheme.bodyText1),
+            body: Column(
+              children: [
+                StreamBuilder<RecordingDisposition>(
+                  builder: (context, snapshot) {
+                    final duration = snapshot.hasData
+                        ? snapshot.data!.duration
+                        : Duration.zero;
+
+                    String twoDigits(int n) => n.toString().padLeft(2, '0');
+
+                    final twoDigitMinutes =
+                        twoDigits(duration.inMinutes.remainder(60));
+                    final twoDigitSeconds =
+                        twoDigits(duration.inSeconds.remainder(60));
+
+                    return Text(
+                      '$twoDigitMinutes:$twoDigitSeconds',
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  },
+                  stream: cubit.recorder.onProgress,
+                ),
+                const SizedBox(height: 10.0),
+                IconButton(
+                  onPressed: () async {
+                    if (cubit.recorder.isRecording) {
+                      await cubit.stopRecorder(context);
+                    } else {
+                      await cubit.startRecord();
+                    }
+                  },
+                  icon: cubit.recorder.isRecording
+                      ? const Icon(Icons.stop, size: 40.0)
+                      : const Icon(Icons.mic, size: 40.0),
+                ),
+              ],
             ),
           );
         },
